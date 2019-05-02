0b1aac3849956dab8e3da2b30e928b0de14cb604
thienphan@beeketing.net <thienphan@beeketing.net>
2019-05-02 10:01:52 +0700

    change sign in error toast message

--- app/src/main/java/com/example/nam_t/datingapp/LoginActivity.java
+++ app/src/main/java/com/example/nam_t/datingapp/LoginActivity.java
@@ -76,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "sign in error !!! ", Toast.LENGTH_LONG).show();
+                                Toast.makeText(LoginActivity.this, "Đã có lỗi, xin thử lại", Toast.LENGTH_LONG).show();
                             }
 
 
